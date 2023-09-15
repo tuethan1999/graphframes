db2dc3e35000ae26e9ed68cda822cbb37fb4b266
Ethan Tu <34871276+tuethan1999@users.noreply.github.com>
2023-09-15 10:46:53 -0700

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,2 +1,6 @@
-# Your python setup file. An example can be found at:
-# https://github.com/pypa/sampleproject/blob/master/setup.py
+
+setup(
+    name='graphframes',
+    version='0.8.2',
+    packages=['graphframes', 'graphframes.lib', 'graphframes.examples']
+)
